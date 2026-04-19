b28e802e2c588a9f376543970d840f88c3d11e54
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2026-04-19 09:00:08 -0700

    Removed FileToCpp middle man

--- Code/Installer/installer.cpp
+++ Code/Installer/installer.cpp
@@ -1,4 +1,4 @@
-#include "PackedData/pd_dll_proxy.hpp"
+#include "../Resources/installer_resources.h"
 #include "Common/game_finder.hpp"
 
 #include "Utils/Console.hpp"
@@ -6,30 +6,28 @@
 
 #include <fstream>
 
-bool write_packed_data(const std::wstring& path, const char* binary_data, std::size_t binary_size)
+static bool WritePackedData(const std::wstring& path, const void* pBinary, const std::size_t binarySz)
 {
-	std::ofstream v_binary_output(path, std::ios::binary);
-	if (!v_binary_output.is_open())
+	std::ofstream v_binOutput(path, std::ios::binary);
+	if (!v_binOutput.is_open())
 	{
 		DebugErrorL("Couldn't write the data to: ", path);
 		return false;
 	}
 
-	v_binary_output.write(binary_data, binary_size);
-	v_binary_output.close();
-
+	v_binOutput.write(reinterpret_cast<const char*>(pBinary), binarySz);
 	return true;
 }
 
-bool run_main_installer(const std::wstring& game_directory)
+static bool RunMainInstaller(const std::wstring& gameDirectory)
 {
-	const std::wstring v_moved_dll = game_directory + L"/vcruntime140_1_.dll";
-	const std::wstring v_proxy_dll = game_directory + L"/vcruntime140_1.dll";
+	const std::wstring v_movedDll = gameDirectory + L"/vcruntime140_1_.dll";
+	const std::wstring v_proxyDll = gameDirectory + L"/vcruntime140_1.dll";
 
-	if (!File::Exists(v_moved_dll))
+	if (!File::Exists(v_movedDll))
 	{
 		DebugOutL("Moving the dll");
-		if (!File::Rename(v_proxy_dll, v_moved_dll))
+		if (!File::Rename(v_proxyDll, v_movedDll))
 		{
 			DebugErrorL("Couldn't move the dll!");
 			return false;
@@ -40,10 +38,24 @@ bool run_main_installer(const std::wstring& game_directory)
 		DebugOutL("DLL is already moved");
 	}
 
-	File::CreateDir(game_directory + L"/DLLModules");
+	File::CreateDir(gameDirectory + L"/DLLModules");
 
-	//Subtract the null character from the binary
-	return write_packed_data(v_proxy_dll, g_proxyDllData, sizeof(g_proxyDllData) - 1);
+	HRSRC v_hRes = FindResource(NULL, MAKEINTRESOURCE(IDR_RCDATA1), RT_RCDATA);
+	if (v_hRes)
+	{
+		HGLOBAL v_hResData = LoadResource(NULL, v_hRes);
+		if (v_hResData)
+		{
+			const void* v_pResData = LockResource(v_hResData);
+			if (v_pResData)
+			{
+				const DWORD v_resDataSz = SizeofResource(NULL, v_hRes);
+				return WritePackedData(v_proxyDll, v_pResData, v_resDataSz);
+			}
+		}
+	}
+
+	return false;
 }
 
 int WINAPI WinMain(
@@ -57,11 +69,11 @@ int WINAPI WinMain(
 
 	if (!GameFinder::IsGameRunning())
 	{
-		std::wstring v_game_path;
-		if (GameFinder::FindGame(v_game_path))
+		std::wstring v_gamePath;
+		if (GameFinder::FindGame(v_gamePath))
 		{
-			DebugOutL("Found the game path: ", v_game_path);
-			if (run_main_installer(v_game_path))
+			DebugOutL("Found the game path: ", v_gamePath);
+			if (RunMainInstaller(v_gamePath))
 			{
 				DebugOutL(0b0101_fg, "Successfully installed the DLL-Injector!");
 			}
@@ -70,10 +82,10 @@ int WINAPI WinMain(
 				DebugErrorL("Something went wrong while installing the DLL-Injector!");
 				DebugErrorL("Make sure you are not running this in the same folder as ScrapMechanic.exe!");
 
-				DWORD v_error_code = GetLastError();
-				if (v_error_code)
+				DWORD v_errorCode = GetLastError();
+				if (v_errorCode)
 				{
-					DebugErrorL("(CODE: ", v_error_code, ") -> ", std::system_category().message(v_error_code));
+					DebugErrorL("(CODE: ", v_errorCode, ") -> ", std::system_category().message(v_errorCode));
 				}
 			}
 		}
